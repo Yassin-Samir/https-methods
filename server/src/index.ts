7d67d757b72b76a4504f1423cd42ce181d4325c2
Mohamed Hossam <81392900+mohamedhoss123@users.noreply.github.com>
2022-09-20 17:33:30 +0200

    fix cors error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express"
 import { AppDataSource } from "./database/datasource"
 import { User } from "./database/manga"
+import cors from "cors"
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 
