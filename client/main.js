a5ba8c99071bc2fcbfe538269a8ce25e754848b2
yassin samir <yassinsam3@gmail.com>
2022-09-20 18:01:28 +0200

    fixing bugs

--- client/main.js
+++ client/main.js
@@ -1,3 +1,24 @@
 (async () => {
-  console.log(await (await fetch("http://localhost:3000/users")).json());
+  const delete1 = await (
+    await fetch("http://localhost:3000/users", {
+      method: "POST",
+      body: JSON.stringify({
+        name: "yassin",
+        age: 13,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  ).json();
+  console.log(delete1);
+  const data2 = await (await fetch("http://localhost:3000/users")).json();
+  console.log(data2);
+  data2.map((i) => {
+    console.log("fs");
+    const p = document.createElement("p");
+    const name = i.Name;
+    p.textContent = name;
+    document.body.append(p);
+  });
 })();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,62 +1,36 @@
-import express from "express"
-import { AppDataSource } from "./database/datasource"
-import { User } from "./database/manga"
-import cors from "cors"
-const app = express()
-app.use(cors())
-app.use(express.json())
+import express from "express";
+import { AppDataSource } from "./database/datasource";
+import { User } from "./database/manga";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.get("/users", async (req, res) => {
+  const data = await User.find();
+  res.send(data);
+});
 
+app.post("/users", async (req, res) => {
+  console.log(req);
+  let { name, age } = req.body
+  let user = new User();
+  user.Name = name;
+  user.age = age;
+  try {
+    await user.save();
+  } catch (e) {}
+  res.send(user);
+});
 
+app.delete("/users", async (req, res) => {});
 
+app.put("/users", async (req, res) => {});
 
-
-
-
-
-
-app.get("/users",async(req,res)=>{
-    const data  = await User.find()
-    res.send(data)
-})
-
-
-
-app.post("/users",async(req,res)=>{
-    console.log( req.body)
-    let {name , age} =  req.body
-    let user = new User()
-    user.Name = name
-    user.age = age
-    try{
-        await user.save()
-    }catch(e){
-
-    }
-    res.send(user)
-})
-
-
-
-app.delete("/users",async(req,res)=>{
-
-})
-
-app.put("/users",async(req,res)=>{
-
-})
-
-
-
-
-
-
-AppDataSource.initialize().then(()=>{
-    app.listen(3000,()=>{
-        console.log("localhost:3000")
-    })
-}).catch(e=>console.log(e))
-
-
-
-
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("localhost:3000");
+    });
+  })
+  .catch((e) => console.log(e));
